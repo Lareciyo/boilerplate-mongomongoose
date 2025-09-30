b052f03d84042a483afb69328df3c58050b56edc
Lareciyo <lmcneal@perseverenow.org>
2025-09-30 09:39:25 -0500

    9:39

--- myApp.js
+++ myApp.js
@@ -6,6 +6,9 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
+   .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 
 let Person;
 
